249d42c75e4affe90f8982d3da4f3710c717a100
HaGa IT Solution <nengkya@yahoo.com>
2026-02-17 03:13:42 +0700

    struct object * struct_object_alias = struct_object;

--- reference_counting.c
+++ reference_counting.c
@@ -4,29 +4,43 @@
 
 struct object {
 
-    size_t reference_counting;
+    size_t reference_count;
 
     char value;
 
 };
 
 
+/*constructor*/
 struct object * object_create(int value) {
 
     struct object * struct_object = (struct object *)malloc(sizeof * struct_object);
 
     /*initial owner*/
-    (* struct_object).reference_counting = 1;
+    (* struct_object).reference_count = 1;
     (* struct_object).value = value;
 
     /*%p void **/
-    printf("create %p (reference = %d)\n", (void *)struct_object, (* struct_object).value);
+    printf("create struct object pointer at %p (reference = %zu)\n", (void *)struct_object, (* struct_object).reference_count);
+
+   return struct_object;
+
+}
+
+
+void object_keep(struct object * struct_object_alias) {
+
+    (* struct_object_alias).reference_count++;
+
+    printf("keep   struct object pointer at %p (reference = %ld)\n", (void *)struct_object_alias, (* struct_object_alias).reference_count);
 
     /*
     struct_object = 10;
     error: assignment to ‘struct object *’ from ‘int’ makes pointer from integer without a cast [-Wint-conversion]
     */
-    free(struct_object);struct_object = (struct object *)0;
+    free(struct_object_alias);struct_object_alias = (struct object *)0;
+
+
 
 
 
@@ -35,9 +49,35 @@ struct object * object_create(int value) {
 }
 
 
+
+
+
+
+
+
+
+
+
 int main() {
 
+    /*initial owner*/
     struct object * struct_object = object_create(42);
+
+    /*second reference*/
+    struct object * struct_object_alias = struct_object;
+
+    object_keep(struct_object_alias);
+
+
+
+
+
+
+
+
+
+
+
 
 
 
